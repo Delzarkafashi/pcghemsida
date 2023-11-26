106a147040a0055a0e9159ee6ddf8c4bd4d582da
Delzarkafashi <delzarkafashi@gmail.com>
2023-11-26 11:58:40 +0100

    börjat och fixa med product/korg men är inte klart

--- src/components/header/Korg.js
+++ src/components/header/Korg.js
@@ -1,21 +1,35 @@
-
 import React from 'react';
+import { useCart } from '../CartContext';
 
- const Korg = () => {
-   return (
+const Korg = () => {
+  const { cartItems, addToCart, removeFromCart } = useCart();
+
+  const handleIncrease = (item) => {
+    addToCart(item);
+  };
+
+  const handleDecrease = (item) => {
+    removeFromCart(item.name);
+  };
+
+  return (
     <div>
-     <h3>Brazilian Jiu-Jitsu (BJJ)</h3>
-      <p>
-        Brazilian Jiu-Jitsu (BJJ) är en kampsport och självförsvarskonst som fokuserar på markkamp
-        och subduering av motståndaren genom grepp och svep. Målet är att använda teknik och smidighet
-         för att övervinna fysisk styrka. BJJ utvecklades från judo och betonar markkontroll,
-         positionering och submission-tekniker som lås och strypningar. Utövare, kända som
-        "praktikanter" eller "grapplers", tävlar genom att försöka få sin motståndare att ge upp
-        genom att använda olika tekniker. BJJ är inte bara en sport utan också en disciplin som
-         främjar fysisk och mental utveckling samt självförtroende.
-      </p>
+      <h2>Din Korg</h2>
+      <ul>
+        {cartItems.map((item, index) => (
+          <li key={index}>
+            {item.name} - Antal: {item.quantity}{' '}
+            <button onClick={() => handleIncrease(item)}>+</button>
+            <button onClick={() => handleDecrease(item)}>-</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 export default Korg;
+
+
+
+
